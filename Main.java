81ffeb3487fe878a73a4e691ba665ddc22b8ca35
OGSLOVIK <pslowik2@icloud.com>
2025-03-02 12:17:11 +0100

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,43 +1,16 @@
-import java.util.Random;
+
 import java.util.Scanner;
-//public class Main {
-//    public static void main(String[] args) {
-//        Scanner scanner = new Scanner(System.in);
-//        System.out.println("Podaj swoje imie: ");
-//        String imie = scanner.nextLine();
-//
-//        System.out.println("Podaj swój wiek: ");
-//        int wiek = scanner.nextInt();
-//
-//        System.out.println("Cześć " +imie +"! Masz " +wiek + " lat.");
-//
-//
-//    }
-//}
-
-//public class Main {
-//    public static void main(String[] args) {
-//        Scanner scanner = new Scanner(System.in);
-//        System.out.println("Podaj pierwszą liczbe: ");
-//        int first = scanner.nextInt();
-//
-//        System.out.println("Podaj drugą liczbę: ");
-//        int second = scanner.nextInt();
-//
-//        int suma = first+second;
-//
-//        System.out.println( "Suma twoich liczb to: " + suma);
-//
-//
-//    }
-//}
-
-
 public class Main {
     public static void main(String[] args) {
-        Random generator = new Random();
-        int losowa liczba = generator.nextInt(100);
-        System.out.println();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj swoje imie: ");
+        String imie = scanner.nextLine();
+
+        System.out.println("Podaj swój wiek: ");
+        int wiek = scanner.nextInt();
+
+        System.out.println("Cześć " +imie +"! Masz " +wiek + " lat.");
+
 
     }
 }
